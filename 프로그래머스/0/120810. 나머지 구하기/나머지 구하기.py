e0ae79cc14fd854dcac7c752c742ec15ae50a1cb
yeontae <g9060g9060@gmail.com>
2025-04-15 16:07:23 +0900

    [level 0] Title: 나머지 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.py
@@ -0,0 +1,7 @@
+def solution(num1, num2):
+    # 1. num1을 num2로 나누기
+    # 2. 나눈 나머지를 return으로 제출.
+    # 3. 정답을 제출 -> answer에 정답을 담아서 제출
+    # 4. 변수에 우리가 원하는 어떤 값(모든)을 넣는 방법은 "=" 이퀄로 해결할 수 있습니다.
+    answer = num1 % num2
+    return answer 
